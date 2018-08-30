7b086fac6ad6c81641981d7113861c81ec0f9b1d
Shipon Sarder <shipon0142@gmail.com>
2018-08-30 16:38:21 +0600

    Splash_animation_added_last_commit(30/08/18)

--- app/src/main/java/com/example/shipon/medicinereminder/activity/SplashActivity.java
+++ app/src/main/java/com/example/shipon/medicinereminder/activity/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.shipon.medicinereminder.activity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.shipon.medicinereminder.R;
 
@@ -15,10 +19,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView  im=findViewById(R.id.sp);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.myanim);
+        im.startAnimation(animation);
         new Thread() {
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(4000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
